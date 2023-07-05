0618680bdff2fe43aff96e0a35ca11d945ffe310
zdong2080 <85309696+zdong2080@users.noreply.github.com>
2023-07-05 17:04:17 -0500

    update

--- src/index.js
+++ src/index.js
@@ -11,3 +11,19 @@ app.get('/', function (req, res) {
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
 }); 
+
+app.get('/products', function (req, res) {
+    console.log('received')
+    var products = {
+        1: {
+            name: "Product 1",
+            description: "This is an awesome product"
+        },
+        2: {
+            name: "Product 2",
+            description: "This is also an awesome product"
+        }
+   };
+   res.writeHead(200, {"Content-Type": "application/json"});
+   res.end(JSON.stringify(products));
+});
